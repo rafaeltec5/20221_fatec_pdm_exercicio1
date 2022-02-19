d5d7ee38846b88680948f9e7ae6204fcf6f79c97
Rafael Ramos <rafaeltec5@gmail.com>
2022-02-18 21:43:02 -0300

    correcao de bug

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -3,7 +3,7 @@ public class TesteCalculadora {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         System.out.println("Somando 1 + 1 = " + calc.soma(1, 1));
-        System.out.println("Subtraindo 2 - 1 = " + calc.soma(2, 1));
+        System.out.println("Subtraindo 2 - 1 = " + calc.subtracao(2, 1));
     }
 
 }
